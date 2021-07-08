aaa75f5aff1aaec85d962d4e2d40fb43b1a304bd
Beatriz677 <86676818+Beatriz677@users.noreply.github.com>
2021-07-07 19:49:12 -0500

    Add files via upload

---
+++ opperator.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+//Section 1 = variables
+
+var secs = 60
+var min = 3
+
+//section 2 = define control Flow
+
+for index in 1...3 {
+    print("\(String(describing: index)) times 60 is \(index * 60)")
+}
